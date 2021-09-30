093cd4ec8e535b08a3c1ce6ecd65630478ecb816
SerDinadan <serdinadan@gmail.com>
2021-09-29 20:07:24 -0700

    Labels and Buttons Layout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,4 +16,26 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
-window = tkinter.Tk()
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+blank = Label(bg=YELLOW)
+blank.grid(column=0, row=0)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
+start = Button(text="Start", font=(FONT_NAME, 12, "bold"))
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", font=(FONT_NAME, 12, "bold"))
+reset.grid(column=2, row=2)
+
+window.mainloop()
